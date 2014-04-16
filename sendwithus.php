7278e6ace31e7ac9f5a77063350225d780c9cac5
Kyle Poole <poolekyle@gmail.com>
2014-04-16 14:37:09 -0700

    API Key is now saved/loaded inside control panel.

--- sendwithus.php
+++ sendwithus.php
@@ -12,18 +12,36 @@ Version: 0.1
 Author URI: http://www.sendwithus.com
 */
 
-
 add_action('admin_menu', 'activate_sidebar_shortcut');
-
 // Creates link to plugin settings in WordPress control panel.
 function activate_sidebar_shortcut() {
 	// Add the shortcut for the plugin settings underneath the 'plugins' sidebar menu.
 	add_submenu_page('plugins.php', 'sendwithus', 'sendwithus', 'manage_options', 'sendwithus_admin_menu', 'sendwithus_conf_main');
+	
+	// Create an area in WordPress to store the settings saved by the user.
+	add_action('admin_init', 'sendwithus_register_settings');
+}
+
+// Used to create an area to save plugin settings.
+function sendwithus_register_settings() {
+	// Save settings within wp_options table as 'sendwithus_settings'
+	register_setting('sendwithus_settings', 'api_key');
+}
+
+function sendwithus_validate_settings($args) {
+	// Used to validate settings passed to the plugin.
+	echo("Sanitized!<br/>");
+	return $args;
 }
 
 // Used for displaying the main menu page.
 // Activated when user clicks on link in sidebar.
 function sendwithus_conf_main() {
+	// Make sure that the user has appropriate permissions to be here.
+	if ( !current_user_can( 'manage_options' ) ) {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+
 	?>
 	<!-- Font for sendwithus' logo -->
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
@@ -38,48 +56,59 @@ function sendwithus_conf_main() {
 
 		<!-- A check should be performed before loading the table to ensure that the user
 			 has entered an API key - otherwise only an entry for API key should be displayed. -->
-
-		<table class="wp-list-table widefat sendwithus_table">
-			<thead>
-				<th>WordPress Event</th>
-				<th>sendwithus Template</th>
-			</thead>
-			<!-- For now this is static, but we should find a way to poll wordpress and gather all the emails  -->
-			<tr>
-				<td>Event Example #1</td>
-				<td>
-					<!-- This should pull from swu to list the available templates -->
-					<select style="width: 100%">
-						<option>Template Example 1</option>
-						<option>Template Example 2</option>
-						<option>Template Example 3</option>
-					</select>
-			</tr>
-			<tr>
-				<td>Event Example #2</td>
-				<td>
-					<!-- This should pull from swu to list the available templates -->
-					<select style="width: 100%">
-						<option>Template Example 1</option>
-						<option>Template Example 2</option>
-						<option>Template Example 3</option>
-					</select>
-			</tr>
-			<tfoot>
+		<form action="options.php" method="post">
+			<?php
+				// Load up the previously saved settings.
+				settings_fields('sendwithus_settings');
+				do_settings_sections('sendwithus_settings');
+			?>
+			<table class="wp-list-table widefat sendwithus_table">
+				<thead>
+					<th>WordPress Event</th>
+					<th>sendwithus Template</th>
+				</thead>
+				<!-- For now this is static, but we should find a way to poll wordpress and gather all the emails  -->
 				<tr>
-					<td>sendwithus API Key</td>
+					<td>Event Example #1</td>
 					<td>
-						<input type="text" name="api_key" id="input_api_key" placeholder="Your sendwithus API key." style="width: 100%" />
+						<!-- This should pull from swu to list the available templates -->
+						<select style="width: 100%">
+							<option>Template Example 1</option>
+							<option>Template Example 2</option>
+							<option>Template Example 3</option>
+						</select>
 					</td>
 				</tr>
-			</tfoot>
-		</table>
-		<div style="width: 100%; margin-left: auto; margin-right: auto; display: block; padding: 0px 0px 10px;">
-			<input type="submit" name="key" id="api_key_settings" class="button button-primary" value="Save Changes" style="margin: 10px 0px; width: 100%"/>
-		</div>
+				<tr>
+					<td>Event Example #2</td>
+					<td>
+						<!-- This should pull from swu to list the available templates -->
+						<select style="width: 100%">
+							<option>Template Example 1</option>
+							<option>Template Example 2</option>
+							<option>Template Example 3</option>
+						</select>
+					</td>
+				</tr>
+				<tfoot>
+					<tr>
+						<td>sendwithus API Key</td>
+						<td>
+							<input type="text" name="api_key" placeholder="Your sendwithus API key." style="width: 100%" 
+								value="<?php echo get_option('api_key'); ?>"/>
+						</td>
+					</tr>
+				</tfoot>
+			</table>
+			<div style="width: 100%; margin-left: auto; margin-right: auto; display: block; padding: 0px 0px 10px;">
+				<!--
+				<input type="submit" name="key" id="api_key_settings" class="button button-primary" value="Save Changes" style="margin: 10px 0px; width: 100%"/>
+				-->
+				<?php submit_button() ?>
+			</div>
+		</form>
 	</div>
 	<?
 }
-
 
 ?>
