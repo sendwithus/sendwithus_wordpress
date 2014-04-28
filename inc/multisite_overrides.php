8393473426a45e5a3c81b8bac38533293f7fc510
Kyle Poole <poolekyle@gmail.com>
2014-04-27 21:37:07 -0700

    Started work on swu_mpu_signup_blog_notification
    Still needs work.

--- inc/multisite_overrides.php
+++ inc/multisite_overrides.php
@@ -7,7 +7,7 @@
 // Solution: Filters!
 
 // Filter for when a new blog is created on a multisite site.
-add_filter ("newblog_notify_siteadmin", "swu_newblog_notify_siteadmin", 10, 1);
+add_filter("newblog_notify_siteadmin", "swu_newblog_notify_siteadmin", 10, 1);
 
 function swu_newblog_notify_siteadmin($msg) {
     $api = new \sendwithus\API($GLOBALS['api_key']);
@@ -39,7 +39,7 @@ function swu_newblog_notify_siteadmin($msg) {
 }
 
 // Filter for when a new user has been activated - notify the network admin.
-add_filter ("newuser_notify_siteadmin", "swu_newuser_notify_siteadmin", 10, 2);
+add_filter("newuser_notify_siteadmin", "swu_newuser_notify_siteadmin", 10, 2);
 
 function swu_newuser_notify_siteadmin($msg, $user) {
     $api = new \sendwithus\API($GLOBALS['api_key']);    
@@ -59,6 +59,41 @@ function swu_newuser_notify_siteadmin($msg, $user) {
                 'site_url' => $options_site_url,
                 'remote_ip' => $remote_ip,
                 'default_message' => htmlDefaultMessage($msg)
+            )
+        )
+    );
+
+    return false;
+}
+
+// Filter for when a new signup has been successful. Used when site registration is enabled.
+add_filter("wpmu_signup_blog_notification_email", "swu_wpmu_signup_blog_notification", 10, 8);
+
+function swu_wpmu_signup_blog_nofitication($content, $domain, $path, $title, $user, $user_email, $key, $meta) {
+    $api = new \sendwithus\API($GLOBALS['api_key']); 
+
+    // Generate the activation link.
+    if ( !is_subdomain_install() || get_current_site()->id != 1 )
+        $activate_url = network_site_url("wp-activate.php?key=$key");
+    else
+        $activate_url = "http://{$domain}{$path}wp-activate.php?key=$key"; // @todo use *_url() API
+    $activate_url = esc_url($activate_url);
+
+    // Get the administrator's email.
+    $admin_email = get_site_option( 'admin_email' );
+    if ( $admin_email == '' )
+        $admin_email = 'support@' . $_SERVER['SERVER_NAME'];
+    $from_name = get_site_option( 'site_name' ) == '' ? 'WordPress' : esc_html( get_site_option( 'site_name' ) );
+
+    // Get the message together
+    $default_message = sprintf($content, $activate_url, esc_url( "http://{$domain}{$path}" ), $key);
+
+    $response = $api->send(
+        get_option('ms_new_user_network_admin'),
+        array('address' => $user_email),
+        array(
+            'email_data' => array(
+                'default_message' => htmlDefaultMessage($default_message)
             )
         )
     );
