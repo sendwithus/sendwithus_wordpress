8f7faac86ef747f6c7ec72b601fe1c0ad653c298
demoore <moore.dylan.e@gmail.com>
2014-04-30 11:11:17 -0700

    Re-did wpmu_welcome_notification and wpmu_welcome_user_notification

--- inc/multisite_overrides.php
+++ inc/multisite_overrides.php
@@ -38,3 +38,70 @@ function swu_newblog_notify_siteadmin($msg) {
     return false;
 }
 
+add_filter("wpmu_welcome_user_notification", "swu_wpmu_welcome_user_notification", 10, 3);
+function swu_wpmu_welcome_user_notification( $user_id, $password, $meta ){
+    $api = new \sendwithus\API($GLOBALS['api_key']);
+
+    $user  = get_userdata( $user_id );
+
+    $admin_email = get_site_option( 'admin_email' );
+
+    if ( $admin_email == '' )
+        $admin_email = 'support@' . $_SERVER['SERVER_NAME'];
+
+    $current_site = get_current_site();
+
+    if ( empty( $current_site->site_name ) )
+        $current_site->site_name = 'WordPress';
+
+    $response = $api->send(
+        get_option('ms_welcome_user_notification'),
+        array('address' => $user->user_email),
+        array(
+            'email_data' => array(
+                'user_email' => $user->user_email,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'password' => $password,
+                'admin_email' => $admin_email,
+                'site_name' => $current_site->site_name,
+            )
+        )
+    );
+
+    return false;
+}
+
+add_filter("wpmu_welcome_notification", "swu_wpmu_welcome_notification", 10, 5);
+function swu_wpmu_welcome_notification($blog_id, $user_id, $password, $title, $meta ) {
+    $api = new \sendwithus\API($GLOBALS['api_key']);
+    $current_site = get_current_site();
+    $url = get_blogaddress_by_id($blog_id);
+    $user = get_userdata( $user_id );
+
+    $admin_email = get_site_option( 'admin_email' );
+
+    if ( $admin_email == '' )
+        $admin_email = 'support@' . $_SERVER['SERVER_NAME'];
+
+    if ( empty( $current_site->site_name ) )
+        $current_site->site_name = 'WordPress';
+
+    $response = $api->send(
+        get_option('ms_welcome_notification'),
+        array('address' => $user->user_email),
+        array(
+            'email_data' => array(
+                'user_email' => $user->user_email,
+                'first_name' => $user->first_name,
+                'last_name' => $user->last_name,
+                'password' => $password,
+                'admin_email' => $admin_email,
+                'site_name' => $current_site->site_name,
+                'site_url' => $url,
+            )
+        )
+    );
+
+    return false;
+}
